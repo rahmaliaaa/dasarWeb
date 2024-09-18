12e522e658980076ac90a743d9750c3a099abe9c
rahmaliaaa <mozachikaa@gmail.com>
2024-09-18 17:14:20 +0700

    Jobsheet 4 : Praktikum 4.7

--- Jobsheet4/struktur_kontrol.php
+++ Jobsheet4/struktur_kontrol.php
@@ -68,3 +68,15 @@ foreach ($nilaiSiswa as $nilai) {
     $avg = $totalNilai / count($nilaiSiswa);
 }
 echo "Rata-rata nilai: $avg <br>";
+
+echo "<br><br>";
+$totalPembelian = 106000;
+$hargaProduk = 120000;
+$diskon = 0.2;
+
+if ($totalPembelian > 100000){
+    $hargaDiskon = $hargaProduk - ($hargaProduk * $diskon);
+    echo "Harga setelah diskon: $hargaDiskon";
+} else {
+    echo "Harga Normal: $hargaProduk";
+}
