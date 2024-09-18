91b4fd6c62364a26dcccc0747e8415d899a532f6
rahmaliaaa <mozachikaa@gmail.com>
2024-09-18 16:55:12 +0700

    praktikum 4.6

--- Jobsheet4/struktur_kontrol.php
+++ Jobsheet4/struktur_kontrol.php
@@ -54,3 +54,18 @@ foreach ($skorUjian as $nilai) {
     }
     echo "Nilai: $nilai (Lulus) <br>";
 }
+
+echo "<br><br>";
+
+$nilaiSiswa = [85, 92, 78, 64, 90, 75, 88, 79, 70, 96];
+$totalNilai = 0;
+
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai >= 92 || $nilai <= 70) {
+        continue;
+    }
+    $totalNilai += $nilai;
+    $avg = $totalNilai / count($nilaiSiswa);
+}
+echo "Rata-rata nilai: $avg <br>";
