b90adaaf8a00f71f889786a61a9d85c0884f8e74
rahmaliaaa <mozachikaa@gmail.com>
2024-09-25 13:17:47 +0700

    Jobsheet 5 : Praktikum 4.5

--- Jobsheet5/function_return.php
+++ Jobsheet5/function_return.php
@@ -4,5 +4,14 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
-echo "Umur saya adalah ". hitungUmur(2005, 2023) ." tahun" // isi sesuai dengan tahun lahir kalian
+function perkenalan ($nama, $salam="Assalamualaikum"){
+    echo $salam, ",";
+    echo "Perkenalkan, nama saya ", $nama,"<br/>";
+    
+    //memanggil fungsi lain
+    echo "Saya berusia ", hitungUmur(1988, 2023) ," tahun <br>";
+    echo "Senang berkenalan dengan anda<br>";
+}
+//memanggil fungsi perkenalan
+    perkenalan("Lia");
 ?>
