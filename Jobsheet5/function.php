b69e95f12a8a92823d6ee1a326faddeed4092ac0
rahmaliaaa <mozachikaa@gmail.com>
2024-09-25 12:33:17 +0700

    Jobsheet 5 : Praktikum 4.1

---
+++ Jobsheet5/function.php
@@ -0,0 +1,11 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Rahmalia<br/>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+?>
