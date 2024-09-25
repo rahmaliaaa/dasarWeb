4b55585a943f4379297e974a23955bde6ccdadfe
rahmaliaaa <mozachikaa@gmail.com>
2024-09-25 13:01:39 +0700

    Jobsheet 5 : Praktikum 4.3

--- Jobsheet5/function.php
+++ Jobsheet5/function.php
@@ -1,15 +1,15 @@
 <?php
 
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam, ", ";
     echo "Perkenalkan, nama saya ", $nama, "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 //memanggil fungsi yang sudah dibuat
-
 perkenalan("Lia", "Hallo");
 echo "<hr>";
+
 $saya = "Rahmalia";
 $ucapanSalam = "Selamat pagi";
-perkenalan($saya, $ucapanSalam);
+perkenalan($saya);
 ?>
