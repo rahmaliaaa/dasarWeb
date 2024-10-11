96fc247b8aad3aba7e9e6029917e96cbca067e10
rahmaliaaa <mozachikaa@gmail.com>
2024-10-11 22:25:22 +0700

    Jobsheet 7 : Praktikum 2,2

--- Jobsheet7/empty.php
+++ Jobsheet7/empty.php
@@ -1,7 +1,13 @@
 <?php
 $myArray = array();
 if (empty($myArray)){
-    echo "Array tidak tedefinisi atau kosong.";
+    echo "Array tidak tedefinisi atau kosong. <br>";
 } else {
-    echo " Array terdefinisi dan tidak kosong.";
+    echo " Array terdefinisi dan tidak kosong. <br>";
+}
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.<br>";
 }
