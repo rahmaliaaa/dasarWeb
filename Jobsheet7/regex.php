27286f97d4c08614c4f4917cb9d511805befc77e
rahmaliaaa <mozachikaa@gmail.com>
2024-10-11 23:11:42 +0700

    Jobsheet 7 : Praktikum 5.3

--- Jobsheet7/regex.php
+++ Jobsheet7/regex.php
@@ -10,7 +10,12 @@ if (preg_match($pattern, $text)) {
 $pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
 $text = 'There are 123 apples.' ;
 if (preg_match($pattern, $text, $matches) ) {
-    echo "Cocokkan: " . $matches[0];
+    echo "Cocokkan:" . $matches[0];
 } else {
     echo "Tidak ada yang cocok!";
 }
+$pattern = '/apple/';
+$replacement = 'banana' ;
+$text = '<br> I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output: "I like banana pie."
