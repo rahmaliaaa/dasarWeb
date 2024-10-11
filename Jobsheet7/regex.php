a76b52d04c31b7b87a82019e6cc71ac8b459e578
rahmaliaaa <mozachikaa@gmail.com>
2024-10-11 23:08:03 +0700

    Jobsheet 7 : Praktikum 5.2

--- Jobsheet7/regex.php
+++ Jobsheet7/regex.php
@@ -3,7 +3,14 @@ $pattern = '/[a-z]/'; // Cocokkan huruf kecil.
 $text = 'This is a Sample Text.';
 
 if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan!";
+    echo "Huruf kecil ditemukan!<br>";
 } else {
-    echo "Tidak ada huruf kecil!";
+    echo "Tidak ada huruf kecil! <br>";
+}
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+$text = 'There are 123 apples.' ;
+if (preg_match($pattern, $text, $matches) ) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
 }
