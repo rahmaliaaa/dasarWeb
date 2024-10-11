257c3052a170f6928bb9d8e5d73c32f40fca8e79
rahmaliaaa <mozachikaa@gmail.com>
2024-10-11 23:01:33 +0700

    Jobsheet 7 : Praktikum 4.1

--- Jobsheet7/html_aman.php
+++ Jobsheet7/html_aman.php
@@ -31,4 +31,16 @@
                 echo "<p>Input tidak boleh kosong!</p>";
             }
         }
-        
+        // Memeriksa apakah input adalah email yang valid
+        $email = $_POST['email'];
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Lanjutkan dengan pengolahan email yang aman
+            echo "<p>Email yang valid : " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8') . "</p>";
+        } else {
+            // Tangani input yang tidak valid
+            echo "<p>Email tidak valid!</p>";
+        }
+    ?>
+ </body>
+</html>
