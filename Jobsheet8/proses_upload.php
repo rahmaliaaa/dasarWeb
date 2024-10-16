c186d09a386d99049f7b497fc85bfe4b0f5702b8
rahmaliaaa <mozachikaa@gmail.com>
2024-10-16 14:35:34 +0700

    Jobsheet 8 : Praktikum 2 soal 8-9

---
+++ Jobsheet8/proses_upload.php
@@ -0,0 +1,32 @@
+<?php
+// Lokasi penyimpanan file yang diunggah
+$targetDirectory = "documents/";
+
+// Periksa apakah direktori penyimpanan ada, jika tidak maka buat
+if (!file_exists($targetDirectory)) {
+    mkdir($targetDirectory, 0777, true);
+}
+
+if ($_FILES['files']['name'][0]) {
+    $totalFiles = count($_FILES['files']['name']);
+
+    // Loop melalui semua file yang diunggah
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['files']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION)); //soal 9
+
+        // Pindahkan file yang diunggah ke direktori penyimpanan
+        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+            echo "File $fileName berhasil diunggah.<br>";
+            if (in_array($fileType, ['jpg', 'jpeg', 'png', 'gif'])) { //sola 9
+                echo '<img src="' . $targetFile . '" width="200" style="height: auto;"><br>';
+            }
+        } else {
+            echo "Gagal mengunggah file $fileName.<br>";
+        }
+    }
+} else {
+    echo "Tidak ada file yang diunggah.<br>";
+}
+?>
