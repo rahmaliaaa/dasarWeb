d674ccd1f32dc4e3e61563cdeec4b7d81b2a6707
rahmaliaaa <mozachikaa@gmail.com>
2024-10-16 14:06:56 +0700

    Jobsheet 8 : Praktikum 1 soal 6

--- Jobsheet8/upload.php
+++ Jobsheet8/upload.php
@@ -4,13 +4,13 @@ if (isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = ["jpg", "jpeg", "png", "gif"];
+    $allowedExtensions = ["txt", "pdf", "doc", "docx"];
     $maxsize = 3*1024*1024; // 3MB
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.<br>";
-            if (in_array($fileType, ['jpg', 'jpeg', 'png', 'gif'])) {
+            if (in_array($fileType, ['txt', 'pdf', 'doc', 'docx'])) {
                 echo '<img src="' . $targetfile . '" width="200" style="height: auto;"><br>';
             }
         } else {
