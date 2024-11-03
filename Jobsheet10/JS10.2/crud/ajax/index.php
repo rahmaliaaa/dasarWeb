6d92a926628490298c482025edb188c4d6921676
rahmaliaaa <mozachikaa@gmail.com>
2024-11-03 12:28:46 +0700

    Jobsheet10.2 : Praktikum 6.2

--- Jobsheet10/JS10.2/crud/ajax/index.php
+++ Jobsheet10/JS10.2/crud/ajax/index.php
@@ -31,26 +31,35 @@ include 'auth.php';
         <h2 class="text-center" style="margin: 30px;">Data Anggota</h2>
 
         <form method="post" class="form-data" id="form-data">
-            <div class="form-group">
-                <label>Nama</label>
-                <input type="hidden" name="id" id="id">
-                <input type="text" name="nama" id="nama" class="form-control" required="true">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="hidden" name="id" id="id">
+                        <input type="text" name="nama" id="nama" class="form-control" required>
+                        <p class="text-danger" id="err_nama"></p>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-group">
+                        <label>Jenis Kelamin</label><br>
+                        <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required> Laki-laki
+                        <input type="radio" name="jenis_kelamin" id="jenkel2" value="P"> Perempuan
+                    </div>
+                    <p class="text-danger" id="err_jenis_kelamin"></p>
+                </div>
             </div>
 
             <div class="form-group">
-                <label>Jenis Kelamin</label><br>
-                <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required="true"> Laki-laki
-                <input type="radio" name="jenis_kelamin" id="jenkel2" value="P"> Perempuan
+                <label for="alamat">Alamat</label>
+                <textarea name="alamat" id="alamat" class="form-control" required></textarea>
+                <p class="text-danger" id="err_alamat"></p>
             </div>
 
             <div class="form-group">
-                <label>Alamat</label>
-                <textarea name="alamat" id="alamat" class="form-control" required="true"></textarea>
-            </div>
-
-            <div class="form-group">
-                <label>No Telepon</label>
-                <input type="number" name="no_telp" id="no_telp" class="form-control" required="true">
+                <label for="no_telp">No Telepon</label>
+                <input type="number" name="no_telp" id="no_telp" class="form-control" required>
+                <p class="text-danger" id="err_no_telp"></p>
             </div>
 
             <div class="form-group">
@@ -59,9 +68,7 @@ include 'auth.php';
                 </button>
             </div>
         </form>
-
         <hr>
-
         <!-- Data Table -->
         <div class="class" id="data"></div>
     </div>
@@ -89,6 +96,45 @@ include 'auth.php';
             // Memuat Data
             $('#data').load('data.php');
         });
+    $("#simpan").click(function() { 
+        var data = $('.form-data').serialize(); 
+        var nama = document.getElementById("nama").value; 
+        var alamat = document.getElementById("alamat").value; 
+        var no_telp = document.getElementById("no_telp").value; 
+        
+        if (nama == "") { 
+            document.getElementById("err_nama").innerHTML = "Nama Harus Diisi"; 
+        } else { 
+            document.getElementById("err_nama").innerHTML = ""; 
+            if (alamat == "") { 
+                document.getElementById("err_alamat").innerHTML = "Alamat Harus Diisi"; 
+            } else { document.getElementById("err_alamat").innerHTML = ""; 
+                if (document.getElementById("jenkel1").checked == false && document.getElementById("jenkel2").checked == false) { 
+                    document.getElementById("err_jenis_kelamin").innerHTML = "Jenis Kelamin Harus Dipilih"; 
+                } else { document.getElementById("err_jenis_kelamin").innerHTML = ""; 
+                    if (no_telp == "") { 
+                        document.getElementById("err_no_telp").innerHTML = "No Telepon Harus Diisi"; 
+                    } else { document.getElementById("err_no_telp").innerHTML = ""; 
+                        if (nama != "" && alamat != "" && (document.getElementById("jenkel1").checked == true || document.getElementById("jenkel2").checked == true) && no_telp != "") { 
+                            $.ajax({ 
+                                type: 'POST', 
+                                url: "form_action.php", 
+                                data: data, success: function() { 
+                                    $('#data').load("data.php"); 
+                                    document.getElementById("id").value = ""; 
+                                    document.getElementById("form-data").reset(); 
+                                }, 
+                                error: function(response) { 
+                                    console.log(response.responseText); 
+                                } 
+                            }); 
+                        }
+                    }
+                }
+            }
+            
+        } 
+    });
     </script>
 </body>
 </html>
