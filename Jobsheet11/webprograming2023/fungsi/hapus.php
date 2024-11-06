0cf20d6f6960bd6c1f438d008503cb3d24fc10d8
rahmaliaaa <mozachikaa@gmail.com>
2024-11-06 16:50:16 +0700

    Jobsheet11 : Praktikum 5.2

--- Jobsheet11/webprograming2023/fungsi/hapus.php
+++ Jobsheet11/webprograming2023/fungsi/hapus.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 if (!empty($_SESSION['username'])) {
     require '../config/koneksi.php';
     require '../fungsi/pesan_kilat.php';
@@ -9,14 +8,29 @@ if (!empty($_SESSION['username'])) {
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
         $query = "DELETE FROM jabatan WHERE id = '$id'";
-
         if (mysqli_query($koneksi, $query)) {
-            pesan('success', "Jabatan Telah Terhapus.");
+            pesan('success', "Jabatan telah terhapus");
         } else {
-            pesan('danger', "Jabatan Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            pesan('danger', "Jabatan tidak terhapus karena : " . mysqli_error($koneksi));
         }
+        header("Location: ../index.php?page=jabatan");
+    } elseif (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        
+        // Hapus data anggota terlebih dahulu
+        $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+        if (mysqli_query($koneksi, $query2)) {
+            // Hapus data user setelah anggota dihapus
+            $query = "DELETE FROM user WHERE id = '$id'";
+            if (mysqli_query($koneksi, $query)) {
+                pesan('success', "Anggota telah terhapus");
+            } else {
+                pesan('danger', "Data login tidak terhapus karena : " . mysqli_error($koneksi));
+            }
+        } else {
+            pesan('warning', "Data anggota tidak terhapus karena : " . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=anggota");
     }
-
-    header("Location: ../index.php?page=jabatan");
 }
 ?>
