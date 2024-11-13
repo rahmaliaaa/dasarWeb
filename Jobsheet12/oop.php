2c068a31280f65b146746e32088fb916de2b38f7
rahmaliaaa <mozachikaa@gmail.com>
2024-11-13 14:01:37 +0700

    Jobsheet12 : Praktikum 1.4

--- Jobsheet12/oop.php
+++ Jobsheet12/oop.php
@@ -1,87 +1,236 @@
 <?php
-class Car{ //soal 1.1
-    public $brand;
-    public function startEngine(){
-        echo "Engine started!";
-    }
-}
-$car1 = new Car();
-$car1->brand = "Toyota";
+// class Car
+// {
+//     private $model;
+//     private $color;
 
-$car2 = new Car();
-$car2->brand = "Honda";
+//     public function __construct($model, $color)
+//     {
+//         $this->model = $model;
+//         $this->color = $color;
+//     }
 
-$car1->startEngine();
-echo $car2->brand;
+//     public function getModel()
+//     {
+//         return $this->model;
+//     }
 
-class Animal{ //soal 1.2
-    protected $name;
+//     public function setColor($color)
+//     {
+//         $this->color = $color;
+//     }
 
-    public function __construct($name){
-        $this->name = $name;
-    }
-    public function eat(){
-        echo $this->name . " is eating. <br>";
-    }
-    public function sleep(){
-        echo $this->name . " is sleeping. <br>";
-    }
-}
-class Cat extends Animal{
-    public function meow (){
-        echo $this->name . " says meow! <br>";
-    }
-}
-class Dog extends Animal{
-    public function bark (){
-        echo $this->name . " says woof! <br>";
-    }
-}
-$cat = new Cat("Whiskers");
-$dog = new Dog("Buddy");
+//     public function getColor()
+//     {
+//         return $this->color;
+//     }
+// }
 
-$cat->eat();
-$dog->sleep();
+// $car = new Car("Toyota", "Blue");
 
-$cat->meow();
-$dog->bark();
+// echo "Model: " . $car->getModel() . "<br>";
+// echo "Color: " . $car->getColor() . "<br>";
 
-interface Shape { //soal 1.3
+// $car->setColor("Red");
+// echo "Updated Color: " . $car->getColor() . "<br>";
+
+
+// class Animal {
+//     protected $name;
+
+//     public function __construct($name) {
+//         $this->name = $name;
+//     }
+
+//     public function eat() {
+//         echo $this->name . " is eating.<br>";
+//     }
+
+//     public function sleep() {
+//         echo $this->name . " is sleeping.<br>";
+//     }
+// }
+
+// class Cat extends Animal {
+//     public function meow() {
+//         echo $this->name . " saya meow!<br>";
+//     }
+// }
+
+// class Dog extends Animal {
+//     public function bark() {
+//         echo $this->name . " saya woof!<br>";
+//     }
+// }
+
+// $cat = new Cat("Whiskers");
+// $dog = new Dog("Buddy");
+
+// $cat->eat();
+// $dog->sleep();
+
+// $cat->meow();
+// $dog->bark();
+
+// interface Shape
+// {
+//     public function calculateArea();
+// }
+
+// class Circle implements Shape
+// {
+//     private $radius;
+
+//     public function __construct($radius)
+//     {
+//         $this->radius = $radius;
+//     }
+
+//     public function calculateArea()
+//     {
+//         return pi() * pow($this->radius, 2);
+//     }
+// }
+
+// class Rectangle implements Shape
+// {
+//     private $width;
+//     private $height;
+
+//     public function __construct($width, $height)
+//     {
+//         $this->width = $width;
+//         $this->height = $height;
+//     }
+
+//     public function calculateArea()
+//     {
+//         return $this->width * $this->height;
+//     }
+// }
+
+// function printArea(Shape $shape)
+// {
+//     echo "Area: " . $shape->calculateArea() . "<br>";
+// }
+
+// $circle = new Circle(5);
+// $rectangle = new Rectangle(4, 6);
+
+// printArea($circle);
+// printArea($rectangle);
+
+// abstract class Shape {
+//     abstract public function calculateArea();
+// }
+
+// class Circle extends Shape {
+//     private $radius;
+
+//     public function __construct($radius) {
+//         $this->radius = $radius;
+//     }
+
+//     public function calculateArea(){
+//         return pi() * pow($this->radius, 2);
+//     }
+// }
+
+// class Rectangle extends Shape {
+//     private $width;
+//     private $height;
+
+//     public function __construct($width, $height) {
+//         $this->width = $width;
+//         $this->height = $height;
+//     }
+
+//     public function calculateArea() {
+//         return $this->width * $this->height;
+//     }
+// }
+
+// $circle = new Circle(5);
+// $rectangle = new Rectangle(4, 6);
+
+// echo "Area of Circle: " . $circle->calculateArea() . "<br>";
+// echo "Area of Rectangle: " . $rectangle->calculateArea() . "<br>";
+
+interface Shape { //soal 1.4
     public function calculateArea();
 }
-
-class Circle implements Shape {
+interface Color {
+    public function getColor();
+}
+class Circle implements Shape, Color {
     private $radius;
+    private $color;
 
-    public function __construct($radius) {
+    public function __construct($radius, $color) {
         $this->radius = $radius;
+        $this->color = $color;
     }
 
     public function calculateArea() {
         return pi() * pow($this->radius, 2);
     }
-}
 
-class Rectangle implements Shape {
-    private $width;
-    private $height;
-
-    public function __construct($width, $height) {
-        $this->width = $width;
-        $this->height = $height;
-    }
-
-    public function calculateArea() {
-        return $this->width * $this->height;
+    public function getColor() {
+        return $this->color;
     }
 }
 
-function printArea(Shape $shape) {
-    echo "Area: " . $shape->calculateArea() . "<br>";
+$circle = new Circle(5, "Blue");
+
+echo "Area of Circle: " . $circle->calculateArea() . "<br>";
+echo "Color of Circle: " . $circle->getColor() . "<br>";
+
+class Car {
+    private $brand;
+
+    public function __construct($brand) {
+        echo "A new car is created.<br>";
+        $this->brand = $brand;
+    }
+
+    public function getBrand() {
+        return $this->brand;
+    }
+
+    public function __destruct() {
+        echo "The car is destroyed.<br>";
+    }
 }
 
-$circle = new Circle(5);
-$rectangle = new Rectangle(4, 6);
+$car = new Car("Toyota");
+echo "Brand: " . $car->getBrand() . "<br>";
 
-printArea($circle);
-printArea($rectangle);
+class Animal {
+    public $name;
+    protected $age;
+    private $color;
+
+    public function __construct($name, $age, $color) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->color = $color;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function getColor() {
+        return $this->color;
+    }
+}
+
+$animal = new Animal("Dog", 3, "Brown");
+
+echo "Name: " . $animal->name . "<br>";
+echo "Age: " . $animal->getAge() . "<br>";
+echo "Color: " . $animal->getColor() . "<br>";
