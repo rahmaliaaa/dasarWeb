ba6b189cff946dff8779e162484ffb9048e68385
rahmaliaaa <mozachikaa@gmail.com>
2024-11-13 14:42:09 +0700

    Jobsheet12 : Praktikum 1.8

--- Jobsheet12/oop.php
+++ Jobsheet12/oop.php
@@ -74,35 +74,7 @@
 
 
 
-class Animal {
-    public $name;
-    protected $age;
-    private $color;
 
-    public function __construct($name, $age, $color) {
-        $this->name = $name;
-        $this->age = $age;
-        $this->color = $color;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function getAge() {
-        return $this->age;
-    }
-
-    public function getColor() {
-        return $this->color;
-    }
-}
-
-$animal = new Animal("Dog", 3, "Brown");
-
-echo "Name: " . $animal->name . "<br>";
-echo "Age: " . $animal->getAge() . "<br>";
-echo "Color: " . $animal->getColor() . "<br>";
 
 
 //interface Shape {
@@ -201,3 +173,33 @@ class Car {
 
 $car = new Car("Toyota");
 echo "Brand: " . $car->getBrand() . "<br>";
+
+class Animal {
+    public $name;
+    protected $age;
+    private $color;
+
+    public function __construct($name, $age, $color) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->color = $color;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function getColor() {
+        return $this->color;
+    }
+}
+
+$animal = new Animal("Dog", 3, "Brown");
+
+echo "Name: " . $animal->name . "<br>";
+echo "Age: " . $animal->getAge() . "<br>";
+echo "Color: " . $animal->getColor() . "<br>";
