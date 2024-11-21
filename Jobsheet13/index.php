e141c004e2261bfa01b0834411aa4fcc009912e1
rahmaliaaa <mozachikaa@gmail.com>
2024-11-21 10:33:40 +0700

    Jobsheet13 : P2

--- Jobsheet13/index.php
+++ Jobsheet13/index.php
@@ -837,55 +837,33 @@
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
 
-    <!-- Main content -->
-    <section class="content">
+    <?php
+    $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
+    switch (strtolower($page)) {
+        case 'dashboard': 
+            include('pages/dashboard.php'); 
+            break;
+        case 'buku': 
+            include('pages/buku.php'); 
+            break;
+        case 'kategori': 
+            include('pages/kategori.php'); 
+            break;
+        case 'user': 
+            include('pages/user.php'); 
+            break;
+        default: 
+            include('pages/404.php'); 
+            break;
+    }
+    ?>
+    <!-- /.content-wrapper -->
+</div>
+<!-- /.content -->
 
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
-
-    </section>
-    <!-- /.content -->
-  </div>
   <!-- /.content-wrapper -->
 
   <footer class="main-footer">
--- Jobsheet13/pages/dashboard.php
+++ Jobsheet13/pages/dashboard.php
@@ -2,12 +2,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            <h1>Dashboard</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
+              <li class="breadcrumb-item active">Dashboard</li>
             </ol>
           </div>
         </div>
@@ -16,3 +16,17 @@
 
     <!-- Main content -->
     <section class="content">
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-12">
+            <div class="card">
+            <div class="card-body">
+                <h4>Selamat Datang</h4>
+                <hr>
+                <p>Selamat Datang <strong>Administrator</strong>. Anda login sebagai <strong>admin</strong>.</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </section>
